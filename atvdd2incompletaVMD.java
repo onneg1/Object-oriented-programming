02da823bcdba854d4e13b0bc685b30fa71791412
Matheus Teixeira <143418613+onneg1@users.noreply.github.com>
2024-03-07 11:17:40 -0300

    atvdd2incompletaVMD.java

---
+++ atvdd2incompletaVMD.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+        
+        String[] Produto = ("Processador Cynix", "Processador Intel", "Processador AMD");
+        
+        float Emin, Emax; //estoque minimo, estoque maximo
+        float Vmd; //vmd = venda media diaria
+        float mes1 = 100, mes2 = 100, mes3 = 100;
+        
+        //mes1+mes2+mes3
+        Vmd = //observar excel. fazer o mesmo processo para todos produtos (uma "função para cada produto."
+        
+	}
+}
