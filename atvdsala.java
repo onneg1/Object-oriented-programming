dbc7077a4b68486fdcfd84d0c0c3b25fb4224f3b
Matheus Teixeira <143418613+onneg1@users.noreply.github.com>
2024-03-07 10:52:59 -0300

    atvdsala.java

---
+++ atvdsala.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+
+		float resultado1;
+		float resultado2;
+		float resultado3;
+		float x = -2;
+		
+		resultado1 = (2 * x) + (3 * x) - (4 * x);
+		resultado2 = (3 * x) / (4 * x) + (5 * x) - (7 * x);
+		resultado3 = (5 * x) + (6 * x) - x + (10 * x);
+		
+		System.out.println("O resultado da equação A: " + resultado1);
+		System.out.println("O resultado da equação B: " + resultado2);
+		System.out.println("O resultado da equação C: " + resultado3);
+	}
+}
