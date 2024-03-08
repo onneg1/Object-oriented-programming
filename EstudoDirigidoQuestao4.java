0a593f9ccbb595c67ec4919826066bd06a280287
Matheus Teixeira <143418613+onneg1@users.noreply.github.com>
2024-03-08 10:42:03 -0300

    EstudoDirigidoQuestao4.java

---
+++ EstudoDirigidoQuestao4.java
@@ -0,0 +1,15 @@
+public class atv {
+    public static void main(String[] args) {
+        String opcao1 = "Saldo da conta e 5000";
+        String opcao2 = "O usuario cadastrado e Marcos descendo a ladeira";
+        String opcao3 = "O usuario sera encaminhado para uma atendente ";
+
+        System.out.println("Primeira opcao: "+opcao1);
+        System.out.println("Segunda opcao: "+opcao2);
+        System.out.println("Terceira opcao: "+opcao3);
+
+
+        System.out.println("Opcao escolhida foi: "+opcao1);
+        
+    }
+}
